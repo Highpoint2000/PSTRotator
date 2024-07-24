6f665d992c5ab93e20a95a38428c76cd5dda90c0
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2024-07-24 12:41:25 +0200

    Update pstrotator_client.js

--- plugins/PSTRotator/pstrotator_client.js
+++ plugins/PSTRotator/pstrotator_client.js
@@ -1,8 +1,8 @@
 /////////////////////////////////////////////////////////////
 ///                                                       ///
-///  PST ROTATOR CLIENT SCRIPT FOR FM-DX-WEBSERVER (V1.0) ///
+///  PST ROTATOR CLIENT SCRIPT FOR FM-DX-WEBSERVER (V1.1) ///
 ///                                                       ///
-///  by Highpoint                last update: 23.07.24    ///
+///  by Highpoint                last update: 24.07.24    ///
 ///                                                       ///
 ///  https://github.com/Highpoint2000/PSTRotator          ///
 ///                                                       ///
@@ -11,8 +11,8 @@
 (() => {
     // Extract WebserverURL and WebserverPORT from the current page URL
     const currentURL = new URL(window.location.href);
-    const WebserverURL = currentURL.hostname; // Get the hostname from the current URL
-    const WebserverPORT = currentURL.port; // Get the port from the current URL 
+    const WebserverURL = currentURL.hostname;
+    const WebserverPORT = currentURL.port;
     const WebsocketPORT = '3000'; // PORT for Websocket/CORS Proxy
 
     // Configuration variables
@@ -20,6 +20,12 @@
     const WEBSOCKET_URL = `ws://${WebserverURL}:${WebsocketPORT}`; // WebSocket URL
     const JQUERY_URL = `https://code.jquery.com/jquery-${JQUERY_VERSION}.min.js`; // URL for jQuery
     const IMAGE_URL = `http://${WebserverURL}:${WebserverPORT}/js/plugins/PSTRotator/Rotor.png`; // URL for background image
+
+    let ctx; // Canvas context
+    let x, y; // Center coordinates of the canvas
+    const radius = 25;
+    let lineAngle = 26;
+    const lineLength = 74;
 
     const PSTRotatorPlugin = (() => {
         // Add CSS styles
@@ -29,8 +35,8 @@
                 width: 81%;
             }
             #containerRotator {
-				position: relative;
-				margin-top: -13%;
+                position: relative;
+                margin-top: -13%;
                 margin-left: 82%; 
             }
             #backgroundRotator {
@@ -48,7 +54,6 @@
                 position: relative;
                 top: -33px;
                 left: -13px;
-
             }
             #innerCircle {
                 top: 50%;
@@ -89,12 +94,11 @@
 
                     let $serverInfocontainerRotator = $('#tuner-name').parent();
                     $serverInfocontainerRotator.removeClass('panel-100').addClass('panel-75').css('padding-left', '20px');
-
                 } else {
                     // containerRotator already exists; no action needed
                 }
             } else {
-                console.error('Element with class "tuner-info" not found');
+                console.error('Element with class "canvas-container" not found');
             }
         }
 
@@ -107,6 +111,79 @@
                 console.error(`Failed to load script: ${url}`);
             };
             document.head.appendChild(script);
+        }
+
+        // Function to get the current color from CSS
+        function getCurrentColor() {
+            return getComputedStyle(document.documentElement).getPropertyValue('--color-5').trim();
+        }
+
+        // Function to draw the circle and lines on the canvas
+        function drawCircleAndLines() {
+            const color = getCurrentColor();
+            if (!ctx) return; // Check if ctx is defined
+
+            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+            ctx.beginPath();
+            ctx.arc(x, y, radius, 0, 2 * Math.PI);
+            ctx.strokeStyle = color;
+            ctx.lineWidth = 2;
+            ctx.stroke();
+            ctx.closePath();
+
+            ctx.font = 'bold 18px Titillium Web, Calibri, sans-serif';
+            ctx.fillStyle = color;
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            ctx.fillText((lineAngle + 90) % 360 + '°', x, y);
+
+            const lineStartX = x + radius * Math.cos(lineAngle * Math.PI / 180);
+            const lineStartY = y + radius * Math.sin(lineAngle * Math.PI / 180);
+            const lineEndX = x + (radius + lineLength) * Math.cos(lineAngle * Math.PI / 180);
+            const lineEndY = y + (radius + lineLength) * Math.sin(lineAngle * Math.PI / 180);
+
+            ctx.beginPath();
+            ctx.moveTo(lineStartX, lineStartY);
+            ctx.lineTo(lineEndX, lineEndY);
+            ctx.strokeStyle = color;
+            ctx.stroke();
+            ctx.closePath();
+        }
+
+        // Function to establish WebSocket connection
+        function loadWebSocket() {
+            const ws = new WebSocket(WEBSOCKET_URL);
+
+            ws.onopen = () => {
+                console.log('WebSocket connection to ' + WEBSOCKET_URL + ' established');
+            };
+
+            ws.onmessage = (event) => {
+                try {
+                    const position = event.data.trim();
+
+                    if (isNaN(position)) {
+                        throw new Error('Invalid position data received');
+                    }
+
+                    if (position >= 0 && position <= 360) {
+                        lineAngle = position - 90;
+                        drawCircleAndLines();
+                    } else {
+                        console.warn('Received position is out of range:', position);
+                    }
+                } catch (error) {
+                    console.error('Error processing WebSocket message:', error);
+                }
+            };
+
+            ws.onerror = (error) => {
+                console.error('WebSocket error:', error);
+            };
+
+            ws.onclose = () => {
+                console.log('WebSocket connection closed');
+            };
         }
 
         // Main functionality
@@ -122,81 +199,12 @@
             addHtmlElements(); // Add HTML elements
 
             const canvas = $('#CanvasRotator')[0];
-            const ctx = canvas.getContext('2d');	
+            ctx = canvas.getContext('2d'); // Set ctx in the global scope
 
-            let x = canvas.width / 2;
-            let y = canvas.height / 2;
-            const radius = 25;
-            let lineAngle = 26;
-            const lineLength = 74;
-
-            // Function to draw the circle and lines on the canvas
-            function drawCircleAndLines() {
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
-                ctx.beginPath();
-                ctx.arc(x, y, radius, 0, 2 * Math.PI);
-                ctx.strokeStyle = '#ED7D31';
-                ctx.lineWidth = 2;
-                ctx.stroke();
-                ctx.closePath();
-
-                ctx.font = 'bold 18px Titillium Web, Calibri, sans-serif';
-                ctx.fillStyle = '#ED7D31';
-                ctx.textAlign = 'center';
-                ctx.textBaseline = 'middle';
-                ctx.fillText((lineAngle + 90) % 360 + '°', x, y);
-
-                const lineStartX = x + radius * Math.cos(lineAngle * Math.PI / 180);
-                const lineStartY = y + radius * Math.sin(lineAngle * Math.PI / 180);
-                const lineEndX = x + (radius + lineLength) * Math.cos(lineAngle * Math.PI / 180);
-                const lineEndY = y + (radius + lineLength) * Math.sin(lineAngle * Math.PI / 180);
-
-                ctx.beginPath();
-                ctx.moveTo(lineStartX, lineStartY);
-                ctx.lineTo(lineEndX, lineEndY);
-                ctx.strokeStyle = '#ED7D31';
-                ctx.stroke();
-                ctx.closePath();
-            }
-
-            // Function to establish WebSocket connection
-            function loadWebSocket() {
-                const ws = new WebSocket(WEBSOCKET_URL);
-
-                ws.onopen = () => {
-                    console.log('WebSocket connection to ' + WEBSOCKET_URL + ' established');
-                };
-
-                ws.onmessage = (event) => {
-                    try {
-                        const position = event.data.trim();
-
-                        if (isNaN(position)) {
-                            throw new Error('Invalid position data received');
-                        }
-
-                        if (position >= 0 && position <= 360) {
-                            lineAngle = position - 90;
-                            drawCircleAndLines();
-                        } else {
-                            console.warn('Received position is out of range:', position);
-                        }
-                    } catch (error) {
-                        console.error('Error processing WebSocket message:', error);
-                    }
-                };
-
-                ws.onerror = (error) => {
-                    console.error('WebSocket error:', error);
-                };
-
-                ws.onclose = () => {
-                    console.log('WebSocket connection closed');
-                };
-            }
+            x = canvas.width / 2;
+            y = canvas.height / 2;
 
             // Execute functions
-            addHtmlElements();
             loadWebSocket();
         }
 
