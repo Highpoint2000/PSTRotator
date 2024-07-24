260212afc5cd9d0fade2228228fb6c43bb391f48
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2024-07-24 17:03:53 +0200

    Update PSTRotator.js

--- plugins/PSTRotator.js
+++ plugins/PSTRotator.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'AZ-Rotor',
-    version: '1.0',
+    version: '1.1',
     author: 'Highpoint',
     frontEndPath: 'PSTRotator/pstrotator_client.js'
 }
