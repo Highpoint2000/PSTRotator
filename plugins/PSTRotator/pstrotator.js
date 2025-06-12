ec89c709da6c4ceebe7f32c16b12e6b3ea838918
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2025-06-12 16:24:13 +0200

    Update pstrotator.js

--- plugins/PSTRotator/pstrotator.js
+++ plugins/PSTRotator/pstrotator.js
@@ -14,7 +14,7 @@ const RotorLimitLineColor = '#808080'; 	// automatisch via Config aktualisiert
 const RotorLimitLineAngle = 129;		// automatisch via Config aktualisiert
 const updateInfo           = true;    	// Version Check an/aus
 
-const plugin_version       = '2.4a';
+const plugin_version       = '2.4b';
 const plugin_path          = 'https://raw.githubusercontent.com/highpoint2000/PSTRotator/';
 const plugin_JSfile        = 'main/plugins/PSTRotator/pstrotator.js';
 const plugin_name          = 'PST Rotator';
